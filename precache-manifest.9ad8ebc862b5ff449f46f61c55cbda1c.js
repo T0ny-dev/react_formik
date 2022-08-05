d61d2309133b6fcf01fa37bd23cf9fdcaee38518
Toni <antonioraweb@gmail.com>
2022-08-05 01:31:42 -0500

    Updates

---
+++ precache-manifest.9ad8ebc862b5ff449f46f61c55cbda1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "607da117e3a22f8bb992",
+    "url": "/static/css/main.48db87d9.chunk.css"
+  },
+  {
+    "revision": "607da117e3a22f8bb992",
+    "url": "/static/js/main.1fec6c38.chunk.js"
+  },
+  {
+    "revision": "541aaf29666231bd7eaf",
+    "url": "/static/js/runtime~main.c18a8b5e.js"
+  },
+  {
+    "revision": "1e667057637422ab7a70",
+    "url": "/static/js/2.445239e3.chunk.js"
+  },
+  {
+    "revision": "0222b4ed3fafae3e17ab",
+    "url": "/static/js/3.50b2d2e5.chunk.js"
+  },
+  {
+    "revision": "0089b66bd820f46d174808daca328714",
+    "url": "/index.html"
+  }
+];
